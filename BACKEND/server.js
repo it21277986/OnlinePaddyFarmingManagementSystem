1929d3d33998e6dedb8606c82fdfc0b23fe54e00
Lakiiith_00 <110513529+it21277986@users.noreply.github.com>
2023-10-08 09:35:58 +0530

    backned

---
+++ BACKEND/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+// Set the port for the server to listen on, using the environment variable or a default value
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Retrieve MongoDB connection URL from environment variables
+const URL = process.env.MONGODB_URL;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.on("error", (error) => {
+  console.error("Error connecting to MongoDB:", error);
+});
+connection.once("open", () => {
+  console.log("MongoDB Connection success!");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on the port number: ${PORT}`);
+});
