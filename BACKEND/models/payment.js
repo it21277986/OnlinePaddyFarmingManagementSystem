2f5db19f3b17ccdb41f94013a24e7e37d130b8e8
it21804410 <it21804410@my.sliit.lk>
2023-10-16 17:07:31 +0530

    payment edit

--- BACKEND/models/payment.js
+++ BACKEND/models/payment.js
@@ -1,23 +1,16 @@
 const mongoose = require('mongoose');
-
-// Define a schema for the "payment" collection in MongoDB
 const Schema = mongoose.Schema;
 
 const paymentSchema = new Schema({
-
-    farmerId : {
-        type : String,
-        required: true
-    },
-    
-    paymentSlip : {
+    farmerId: {
         type: String,
         required: true
-    }
-    
-})
+    },
+    paymentSlip: {
+        data: Buffer,
+        contentType: String,
+       }
 
-// Create a model named "Payment" based on the defined schema
-const payment = mongoose.model("Payment", paymentSchema);
-
+});
+const payment = mongoose.model("payment",paymentSchema);
 module.exports = payment;
--- BACKEND/routes/payment.js
+++ BACKEND/routes/payment.js
@@ -1,4 +1,77 @@
 const router = require("express").Router();
+let UploadSlip = require("../models/payment");
+const multer = require("multer");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) =>
+        cb(null, file.originalname)
+})
+
+const upload = multer({ storage: storage });
+
+router.route("/addDepositSlip").post(upload.single('slip'), (req, res) => {
+    
+    const newUploadSlip = new UploadSlip({
+        slip: {
+            data: fs.readFileSync(req.file.path),
+            contentType: req.file.mimetype
+        },
+        farmerId: req.body.farmerId,
+        
+    });
+
+    newPayment.save()
+        .then(() => {
+            res.json("Payment Added");
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: "Server error" });
+        });
+});
+
+router.route("/getDepositSlip").get(async (req, res) => {
+    try {
+        const allData = await UploadSlip.find();
+        if (!allData || allData.length === 0) {
+            return res.status(404).json({ error: "No data found" });
+        }
+        res.json(allData);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+router.route("/getDepositSlip/:Did").get(async (req, res) => {
+    let paymentSlip = req.params.Did;
+    const slip = await payment.findOne({ _id: bankSlip }).then((slip) => {
+        res.json(slip) // success
+    }).catch((err) => { // unsuccessful
+        console.log(err);
+    });
+});
+router.route("/getSlipByReqId/:farmerId").get(async (req, res) => {
+    let farmerId = req.params.farmerId;
+    try {
+      const slip = await payment.findOne({ farmerId: farmerId });
+      
+      if (!slip) {
+        return res.status(404).json({ error: "No data found" });
+      }
+      res.json(slip);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      res.status(500).json({ error: "Server error" });
+    }
+  });
+module.exports = router;
+
+/*const router = require("express").Router();
 let Society = require("../models/payment");
 
 //create oparation
@@ -68,4 +141,4 @@ router.route("/getPayment/:pId").get(async(req, res)=>{
     })
 })
 
-module.exports = router;
+module.exports = router;*/
