7a906928cd5d3127ec09800282cfec6f27477687
it21804410 <it21804410@my.sliit.lk>
2023-10-16 19:53:56 +0530

    payment edit 3

--- BACKEND/routes/payment.js
+++ BACKEND/routes/payment.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-let UploadSlip = require("../models/payment");
+let payment = require("../models/payment");
 const multer = require("multer");
 const fs = require("fs");
 
@@ -13,10 +13,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.route("/addDepositSlip").post(upload.single('slip'), (req, res) => {
+router.route("/addDepositSlip").post(upload.single('payemtnSlip'), (req, res) => {
     
-    const newUploadSlip = new UploadSlip({
-        slip: {
+    const newPayment = new payment({
+        paymentSlip: {
             data: fs.readFileSync(req.file.path),
             contentType: req.file.mimetype
         },
@@ -36,7 +36,7 @@ router.route("/addDepositSlip").post(upload.single('slip'), (req, res) => {
 
 router.route("/getDepositSlip").get(async (req, res) => {
     try {
-        const allData = await UploadSlip.find();
+        const allData = await payment.find();
         if (!allData || allData.length === 0) {
             return res.status(404).json({ error: "No data found" });
         }
@@ -49,7 +49,7 @@ router.route("/getDepositSlip").get(async (req, res) => {
 
 router.route("/getDepositSlip/:Did").get(async (req, res) => {
     let paymentSlip = req.params.Did;
-    const slip = await payment.findOne({ _id: bankSlip }).then((slip) => {
+    const slip = await payment.findOne({ _id: paymentSlip }).then((paymentSlip) => {
         res.json(slip) // success
     }).catch((err) => { // unsuccessful
         console.log(err);
@@ -58,87 +58,16 @@ router.route("/getDepositSlip/:Did").get(async (req, res) => {
 router.route("/getSlipByReqId/:farmerId").get(async (req, res) => {
     let farmerId = req.params.farmerId;
     try {
-      const slip = await payment.findOne({ farmerId: farmerId });
+      const paymentSlip = await payment.findOne({ farmerId: farmerId });
       
-      if (!slip) {
+      if (!paymentSlip) {
         return res.status(404).json({ error: "No data found" });
       }
-      res.json(slip);
+      res.json(paymentSlip
+        );
     } catch (error) {
       console.error("Error fetching data:", error);
       res.status(500).json({ error: "Server error" });
     }
   });
 module.exports = router;
-
-/*const router = require("express").Router();
-let Society = require("../models/payment");
-
-//create oparation
-router.route("/addPayment").post((req,res)=>{ 
-    const farmerId = req.body.farmerId;
-    const paymentSlip = req.body.paymentSlip;
-    
-
-    const newPayment = new Payment({
-        farmerId,
-        paymentSlip
-    })
-
-    newPayment.save().then(()=>{
-        res.json("Payment Added")
-    }).catch((err)=>{
-        console.log(err);
-    })
-})
-
-//display oparation(ALL)
-router.route("/Payment").get((req,res)=>{
-    Payment.find().then((payment)=>{
-        res.json(payment)
-    }).catch((err)=>{
-        console.log(err)
-    })
-})
-
-//update oparation
-router.route("/updatePayment/:pId").put(async(req,res)=>{
-    let paymentId = req.params.sId;
-    const {farmerId,paymentSlip} = req.body;
-
-    const updatePayment = {
-        farmerId,
-        paymentSlip
-    }
-    const update = await Payment.findByIdAndUpdate(paymentId, updatePayment).then(()=>{
-        res.status(200).send({status: "Payment details updated"})
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).send({status: "Error with updating data",error: err.massage});
-    })
-})
-
-//delete oparation
-router.route("/deletePayment/:sId").delete(async(req,res)=>{
-    let paymentId = req.params.pId;
-
-    await Payment.findByIdAndDelete(paymentId).then(()=>{
-        res.status(200).send({status: "Payment Deleted"});
-    }).catch((err)=>{
-        console.log(err.massage);
-        res.status(500).send({status: "Error with delete payment",error: err.massage});
-    })
-})
-
-//display (ONE)
-router.route("/getPayment/:pId").get(async(req, res)=>{
-    let paymentId = req.params.pId;
-    const user = await Society.findById(paymentId).then((payment)=>{
-        res.status(200).send({status: "Payment fetched", payment})
-    }).catch(()=>{
-        console.log(err.massage);
-        res.status(500).send({status: "Error with get payment",error: err.massage})
-    })
-})
-
-module.exports = router;*/
