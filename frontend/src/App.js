2702284d95178c795991fcc29efefcd10e650c23
VISHARADDS <118560818+VISHARADDS@users.noreply.github.com>
2023-10-17 06:49:48 +0530

    app. js pesticide imports added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,19 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Sammani
+import CropProtection from "./components/sammani/CropProtection";
+import ProductDisplay from "./components/sammani/ProductsDisplay";
+import ViewProduct from "./components/sammani/viewProduct.js"
+import PaymentUI from "./components/sammani/PaymentUI";
+import HeaderOfficer from "./components/sammani/PesticideDashBoard.js";
+import DashBoardFooter from './components/sammani/DashBoardFooter';
+import Product from './components/sammani/Product.js'
+import PesticideDashBoard from "./components/sammani/PesticideDashBoard.js";
+import Stock from "./components/sammani/Stock"
+import ProductRequest from './components/sammani/ProductRequest.js';
+import PesticidePayment from "./components/sammani/PesticidePayment";
+import UpdateProduct from "./components/sammani/UpdateProduct";
 
 function App() {
   return (
