ec9d1bbc45c14d428078bd174c20d7a74515425c
Lakiiith_00 <110513529+it21277986@users.noreply.github.com>
2023-10-17 09:03:43 +0530

    society file add 2

---
+++ frontend/src/components/Society/DisplaySociety.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DisplaySociety() {
+
+
+    return (
+        <div>
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                
+            </div>
+        </div>
+    )
+}
+
+export default DisplaySociety;
